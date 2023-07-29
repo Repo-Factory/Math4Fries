b6579c4fe08a7af66c9edd983348102d7fd078b2
4n3m4i1 <adrian.de.vico@gmail.com>
2023-07-29 03:28:26 -0700

    Swapped to developing matrix library to support everything better

--- Matrix_Utilities_4_C/matrix_library_basis.c
+++ Matrix_Utilities_4_C/matrix_library_basis.c
@@ -11,13 +11,14 @@
 
 
 struct Matrix_t {
-    uint16_t    dimension;
+    uint8_t     dimension_x;
+    uint8_t     dimension_y;
     uint32_t    presence_bitfield;
     float       **contents;
 };
 
 
-struct Matrix_t *Create_Matrix(uint16_t dimension);
+struct Matrix_t *Create_Matrix(uint8_t dimension_x, uint8_t dimension_y);
 int             Destroy_Matrix(struct Matrix_t *cool_matrix);
 void            Print_Matrix(struct Matrix_t *cool_matrix);
 
@@ -27,20 +28,28 @@ void main(int argc, char **argv){
 
     struct Matrix_t *wow_a_matrix;
 
-    if(argc > 1){
+    if(argc == 2){
         uint16_t idk = (argv[1])[0] - '0';
         if(idk){
-            wow_a_matrix = Create_Matrix(idk);
+            wow_a_matrix = Create_Matrix(idk, idk);
         } else {
-            wow_a_matrix = Create_Matrix(DIM);
+            wow_a_matrix = Create_Matrix(DIM, DIM);
+        }
+    } else if(argc == 3){
+        uint16_t idk = (argv[1])[0] - '0';
+        uint16_t idk2 = (argv[2])[0] - '0';
+        if(idk && idk2){
+            wow_a_matrix = Create_Matrix(idk, idk2);
+        } else {
+            wow_a_matrix = Create_Matrix(DIM, DIM);
         }
     } else {
-        wow_a_matrix = Create_Matrix(DIM);
+        wow_a_matrix = Create_Matrix(DIM, DIM);
     }
     
 
-    for(uint16_t n = 0; n < wow_a_matrix->dimension; n++){
-        for(uint16_t m = 0; m < wow_a_matrix->dimension; m++){
+    for(uint16_t n = 0; n < wow_a_matrix->dimension_y; n++){
+        for(uint16_t m = 0; m < wow_a_matrix->dimension_x; m++){
             wow_a_matrix->contents[n][m] = (float)(n * m);
         }
     }
@@ -53,15 +62,16 @@ void main(int argc, char **argv){
 }
 
 
-struct Matrix_t *Create_Matrix(uint16_t dimension){
-    float** cool_array_space = malloc(dimension * sizeof(float *));
-    for (int n = 0; n < dimension; ++n) {
-        cool_array_space[n] = malloc(dimension * sizeof(float));
+struct Matrix_t *Create_Matrix(uint8_t dimension_x, uint8_t dimension_y){
+    float** cool_array_space = malloc(dimension_y * sizeof(float *));
+    for (int n = 0; n < dimension_y; ++n) {
+        cool_array_space[n] = malloc(dimension_x * sizeof(float));
     }
     
     struct Matrix_t *cool_matrix = (struct Matrix_t *)malloc(sizeof(struct Matrix_t));
 
-    cool_matrix->dimension = dimension;
+    cool_matrix->dimension_x = dimension_x;
+    cool_matrix->dimension_y = dimension_y;
     cool_matrix->presence_bitfield = 0;
     cool_matrix->contents = cool_array_space;
     
@@ -71,7 +81,7 @@ struct Matrix_t *Create_Matrix(uint16_t dimension){
 int Destroy_Matrix(struct Matrix_t *cool_matrix){
     int retval = 0;
 
-    for (int n = 0; n < cool_matrix->dimension; ++n) {
+    for (int n = 0; n < cool_matrix->dimension_y; ++n) {
         free(cool_matrix->contents[n]);
     }
 
@@ -82,11 +92,11 @@ int Destroy_Matrix(struct Matrix_t *cool_matrix){
 }
 
 void Print_Matrix(struct Matrix_t *cool_matrix){
-    printf("Contents of Array, size: %2u\n", cool_matrix->dimension);
+    printf("Contents of Array, size: %u x %u\n", cool_matrix->dimension_x, cool_matrix->dimension_y);
 
-    for(uint16_t n = 0; n < cool_matrix->dimension; n++){
+    for(uint16_t n = 0; n < cool_matrix->dimension_y; n++){
         printf("%2u -> ", n);
-        for(uint16_t m = 0; m < cool_matrix->dimension; m++){
+        for(uint16_t m = 0; m < cool_matrix->dimension_x; m++){
             printf("%.3f\t", cool_matrix->contents[n][m]);
         }
         printf("\n");
